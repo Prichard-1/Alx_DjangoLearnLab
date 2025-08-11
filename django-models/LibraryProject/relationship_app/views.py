d3e98954d8489c2a4f0fb875a49065cf3743d32e
Prichard Dube <genarodube8@gmail.com>
2025-08-11 20:36:15 +0200

    updating views to enforce permission

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,8 +12,10 @@ from .forms import BookForm
 def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
+
 def is_librarian(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
 
 def is_member(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
