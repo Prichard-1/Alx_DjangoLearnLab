fff83f1cee8c662dfb8a30c532daf7b879622d3a
Prichard Dube <genarodube8@gmail.com>
2025-08-11 21:51:28 +0200

    Implementing a Custom User Model in Django mandatory

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -111,4 +111,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ✅ Custom User Model
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
