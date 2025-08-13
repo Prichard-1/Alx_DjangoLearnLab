d8eb8a72489343507c3ebc59398c4406963ffe6d
pricharddube <genarodube8@gmail.com>
2025-08-13 12:46:33 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,10 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
 from .forms import ExampleForm
-
+def book_list(request):
+    
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
