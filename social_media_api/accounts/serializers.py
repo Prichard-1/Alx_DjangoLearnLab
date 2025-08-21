13ac0e30d19ee3b6714a596f9b92dd3abba30e17
Prichard Dube <genarodube8@gmail.com>
2025-08-21 11:32:55 +0200

    fix: update RegisterSerializer to use get_user_model().objects.create_user for checker compliance

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,27 +1,18 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return {'token': token.key}
-        raise serializers.ValidationError("Invalid credentials")
-
