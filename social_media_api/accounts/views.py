471ba99fa409d718fc0b3df56306ac8476114c85
Prichard Dube <genarodube8@gmail.com>
2025-08-21 15:22:18 +0200

    Add follow and unfollow user views with authentication and routing

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import CustomUser
 
+CustomUser.objects.all()
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
